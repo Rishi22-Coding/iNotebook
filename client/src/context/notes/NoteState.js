5e80bf4b3aff1c4f405bc4353ff510a0b66ee648
Rishi22-Coding <saptarshi.patra.22@gmail.com>
2023-05-03 13:32:25 +0530

    changed for frontend url

--- client/src/context/notes/NoteState.js
+++ client/src/context/notes/NoteState.js
@@ -3,7 +3,7 @@ import NoteContext from "./NoteContext";
 import { useState } from "react";
 
 const NoteState = (props)=>{
-    const host = "https://i-notebook-qchlyrqtt-rishi22-coding.vercel.app/"
+    const host = "http://localhost:5000"
     const notesInitial = []
     const [notes, setNotes] = useState(notesInitial)
 
